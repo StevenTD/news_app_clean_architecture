3a113f53eb9b1bda3103bb9319e8f4700ccf6055
StevenTD <steven.delarosa566@gmail.com>
2024-10-30 11:41:56 +0800

    Initial commit

---
+++ lib/features/daily_news/presentation/widgets/article_tile.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class ArticleWidget extends StatelessWidget {
+  final ArticleEntity article;
+  const ArticleWidget({super.key, required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsetsDirectional.only(start: 14, end: 14, bottom: 14),
+      height: MediaQuery.of(context).size.width / 2.2,
+      child: Row(
+        children: [
+          _buildImage(context),
+          _buildTitleAndDescription(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImage(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: article.urlToImage!,
+      imageBuilder: (context, imageProvider) => Padding(
+        padding: const EdgeInsetsDirectional.only(end: 14),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width / 3,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.08),
+              image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+            ),
+          ),
+        ),
+      ),
+      progressIndicatorBuilder: (context, url, downloadProgres) => Padding(
+        padding: const EdgeInsetsDirectional.only(end: 14),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width / 3,
+            height: double.maxFinite,
+            child: const CupertinoActivityIndicator(),
+            decoration: BoxDecoration(color: Colors.black.withOpacity(0.08)),
+          ),
+        ),
+      ),
+      errorWidget: (context, url, error) => Padding(
+        padding: const EdgeInsetsDirectional.only(end: 14),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width / 3,
+            height: double.maxFinite,
+            child: const Icon(Icons.refresh),
+            decoration: BoxDecoration(color: Colors.black.withOpacity(0.08)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildTitleAndDescription() {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 7),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Title
+            Text(
+              article!.title ?? '',
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 18,
+                  color: Colors.black87),
+            ),
+
+            // Description
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.only(top: 4),
+              child: Text(
+                article!.description! ?? '',
+                maxLines: 2,
+              ),
+            )),
+
+            // Date Time
+            Row(
+              children: [
+                const Icon(
+                  Icons.timeline_outlined,
+                  size: 16,
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  article!.publishedAt!,
+                  style: TextStyle(fontSize: 12),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/injection_container.dart
@@ -0,0 +1,30 @@
+import 'package:get_it/get_it.dart';
+import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/local/app_database.dart';
+import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:news_app_clean_architecture/features/daily_news/data/repository/article_repository_impl.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+
+final s1 = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  final database =
+      await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+
+  s1.registerSingleton<AppDatabase>(database);
+
+  // Dio
+  s1.registerSingleton<Dio>(Dio());
+
+  // Dependencies
+  s1.registerSingleton<NewsApiService>(NewsApiService(s1()));
+
+  s1.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(s1()));
+
+  s1.registerSingleton<GetArticleUseCase>(GetArticleUseCase(s1()));
+
+  // Blocks
+  s1.registerFactory<RemoteArticlesBloc>(() => RemoteArticlesBloc(s1()));
+}
